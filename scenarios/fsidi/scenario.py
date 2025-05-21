75dda169235597f76a4cddbcf98e988e50769702
lgactna <lgonzalesna@gmail.com>
2025-05-20 21:10:24 -0700

    linting

--- scenarios/fsidi/scenario.py
+++ scenarios/fsidi/scenario.py
@@ -12,7 +12,6 @@ that is included in this directory. Note that the process of copying the agent m
 take a while.
 """
 
-import datetime
 import logging
 import random
 import sys
@@ -24,11 +23,11 @@ from akflib.rendering.core import bundle_to_pdf, get_pandoc_path, get_renderer_c
 from akflib.rendering.objs import AKFBundle
 
 from akf_windows.api.artifacts import WindowsArtifactServiceAPI
-from akf_windows.api.chromium import ChromiumServiceAPI
 from akf_windows.api.autogui import PyAutoGuiServiceAPI
+from akf_windows.api.chromium import ChromiumServiceAPI
 
 # Change this based on what you've named the resulting VirtualBox machine.
-MACHINE_NAME = "akf-windows-3"
+MACHINE_NAME = "akf-windows-4"
 
 # Set up logging
 logging.basicConfig(
@@ -51,74 +50,82 @@ akf_bundle = AKFBundle()
 logger.info(f'Using virtual machine "{MACHINE_NAME}"')
 vbox_obj = VBoxHypervisor(MACHINE_NAME, akf_bundle)
 
-# # Start the machine
-# logger.info("Starting the VM and enabling network capturing")
-# vbox_obj.start_network_capture(Path("traffic.pcap"))
-# vbox_obj.start_vm(wait_for_guest_additions=True)
+# Start the machine
+logger.info("Starting the VM and enabling network capturing")
+vbox_obj.start_network_capture(Path("traffic.pcap"))
+vbox_obj.start_vm(wait_for_guest_additions=True)
 
-# with ChromiumServiceAPI.auto_connect(vbox_obj.get_maintenance_ip()) as chromium_service:
-#     # Visit a list of websites from a file with some jitter, and save their screenshots
-#     # to the Downloads folder
-#     logger.info("Visiting reddit websites")
-    
-#     url_file = Path("cat_urls.txt")
-#     with open(url_file, "rt") as f:
-#         urls = [line.strip() for line in f.readlines()]
-#     chromium_service.kill_edge()
-#     chromium_service.set_browser("msedge")
-#     page = chromium_service.browser.new_page()
+with ChromiumServiceAPI.auto_connect(vbox_obj.get_maintenance_ip()) as chromium_service:
+    # Visit a list of websites from a file with some jitter, and save their screenshots
+    # to the Downloads folder
+    logger.info("Visiting reddit websites")
 
-#     # Visit these sites in order
-#     for idx, url in enumerate(urls, start=1):
-#         logger.info(f"Visiting {url} and generating screenshot")
-#         page.goto(url)
-#         page.screenshot(path=f"C:/Users/user/Downloads/cat_page_{idx}.png", full_page=True)
-#         time.sleep(60 + random.randint(-15, 15))
-    
-#     # Now visit a suspicious looking link directly
-#     logger.info("Downloading ransomware")
-#     with ChromiumServiceAPI.auto_connect(vbox_obj.get_maintenance_ip()) as chromium_service:
-#         page.goto("https://www.google.com/search?q=free+cat+wallpapers")
-#         page.goto("https://pastebin.com/C7jhGFSr")
-#         page.goto("https://drive.google.com/file/d/1EdYkhP0C6ouo1sh2N8JAUerNiHd5LYFK/view?usp=sharing")
-#         # Manually visit next page - we won't click on the link since it opens a new tab,
-#         # but as far as artifacts go, the effect is the largely the same
-#         page.goto("https://drive.usercontent.google.com/download?id=1EdYkhP0C6ouo1sh2N8JAUerNiHd5LYFK&export=download&authuser=0")
-#         with page.expect_download() as download_info:
-#             page.locator("[type='submit']").click()
-#             download = download_info.value
-#             download.save_as(rf"C:\Users\user\Downloads\{download.suggested_filename}")
+    url_file = Path("cat_urls.txt")
+    with open(url_file, "rt") as f:
+        urls = [line.strip() for line in f.readlines()]
+    chromium_service.kill_edge()
+    chromium_service.set_browser("msedge")
+    page = chromium_service.browser.new_page()
 
-# # Now manually open Explorer to open the file - we can trivially execute this
-# # through guest additions or by opening the command prompt since we know where
-# # the file is, but that's not how a user normally does it. For the sake of an
-# # example, this is how we'd use PyAutoGUI to do it.
-# logger.info("Running ransomware")
-# with PyAutoGuiServiceAPI.auto_connect(vbox_obj.get_maintenance_ip()) as autogui_service:    
-#     autogui_service.pyautogui.hotkey("win", "e")
-#     time.sleep(2)
+    # Visit these sites in order
+    for idx, url in enumerate(urls, start=1):
+        logger.info(f"Visiting {url} and generating screenshot")
+        page.goto(url)
+        page.screenshot(
+            path=f"C:/Users/user/Downloads/cat_page_{idx}.png", full_page=True
+        )
+        time.sleep(60 + random.randint(-15, 15))
 
-#     # Focus address bar
-#     autogui_service.pyautogui.hotkey("ctrl", "l")
-#     autogui_service.pyautogui.typewrite(r"C:\Users\user\Downloads")
-#     autogui_service.pyautogui.press("enter")
-#     time.sleep(2)
+    # Now visit a suspicious looking link directly
+    logger.info("Downloading ransomware")
+    with ChromiumServiceAPI.auto_connect(
+        vbox_obj.get_maintenance_ip()
+    ) as chromium_service:
+        page.goto("https://www.google.com/search?q=free+cat+wallpapers")
+        page.goto("https://pastebin.com/C7jhGFSr")
+        page.goto(
+            "https://drive.google.com/file/d/1EdYkhP0C6ouo1sh2N8JAUerNiHd5LYFK/view?usp=sharing"
+        )
+        # Manually visit next page - we won't click on the link since it opens a new tab,
+        # but as far as artifacts go, the effect is the largely the same
+        page.goto(
+            "https://drive.usercontent.google.com/download?id=1EdYkhP0C6ouo1sh2N8JAUerNiHd5LYFK&export=download&authuser=0"
+        )
+        with page.expect_download() as download_info:
+            page.locator("[type='submit']").click()
+            download = download_info.value
+            download.save_as(rf"C:\Users\user\Downloads\{download.suggested_filename}")
 
-#     # Search for file
-#     autogui_service.pyautogui.hotkey("ctrl", "f")
-#     autogui_service.pyautogui.typewrite("cats.exe")
-#     autogui_service.pyautogui.press("enter")
-#     time.sleep(2)
+# Now manually open Explorer to open the file - we can trivially execute this
+# through guest additions or by opening the command prompt since we know where
+# the file is, but that's not how a user normally does it. For the sake of an
+# example, this is how we'd use PyAutoGUI to do it.
+logger.info("Running ransomware")
+with PyAutoGuiServiceAPI.auto_connect(vbox_obj.get_maintenance_ip()) as autogui_service:
+    autogui_service.pyautogui.hotkey("win", "e")
+    time.sleep(2)
 
-#     # Focus and open file
-#     autogui_service.pyautogui.press("f6")
-#     autogui_service.pyautogui.press("f6")
-#     autogui_service.pyautogui.press("down")
-#     autogui_service.pyautogui.press("enter")
-#     time.sleep(2)
+    # Focus address bar
+    autogui_service.pyautogui.hotkey("ctrl", "l")
+    autogui_service.pyautogui.typewrite(r"C:\Users\user\Downloads")
+    autogui_service.pyautogui.press("enter")
+    time.sleep(2)
 
-# # Collect volatile memory
-# logger.info("Creating memory dump")
+    # Search for file
+    autogui_service.pyautogui.hotkey("ctrl", "f")
+    autogui_service.pyautogui.typewrite("cats.exe")
+    autogui_service.pyautogui.press("enter")
+    time.sleep(2)
+
+    # Focus and open file
+    autogui_service.pyautogui.press("f6")
+    autogui_service.pyautogui.press("f6")
+    autogui_service.pyautogui.press("down")
+    autogui_service.pyautogui.press("enter")
+    time.sleep(2)
+
+# Collect volatile memory
+logger.info("Creating memory dump")
 vbox_obj.create_memory_dump(Path("memory.dmp"))
 
 # Collect Edge artifacts
@@ -137,7 +144,7 @@ with WindowsArtifactServiceAPI.auto_connect(
 
 # Stop the virtual machine; end network capture
 logger.info("Tearing down the VM and finishing packet capture")
-# vbox_obj.stop_vm(force=False)
+vbox_obj.stop_vm(force=False)
 vbox_obj.stop_network_capture()
 
 # Export the virtual machine to a disk image
